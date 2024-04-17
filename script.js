cb4acfc4f6a5a8882a63acd610ef75da0c334902
Vu My <tranhoangvumy22@gmail.com>
2024-04-17 19:42:58 -0400

    Add new code

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const name = document.querySelector(".name span");
-const intro = document.querySelector(".intro");
-const nameText = "MY NAME IS VU MY"
-callback();
-function textTypingEffect(intro, e, text, i = 0) {
-    if (i == 0) {
-        e.textContent = "";
-        intro.textContent = "";
-    }
-    if (i >= 0 && i <= 7) intro.textContent += text[i];
-    else e.textContent += text[i];
-    if (i == text.length - 1) return;
-    setTimeout(() => textTypingEffect(intro, e, text, i + 1), 200);
-}
-function callback() {
-    textTypingEffect(intro, name, nameText);
 
-}
-setInterval(callback, 9000);
+
+
+const menu = document.querySelector(".icon-bar");
+const navbar = document.querySelector("nav");
+let isShow = false;
+menu.addEventListener('click', function () {
+    if (!isShow) {
+        navbar.style.left = "0";
+        menu.style.transform = "rotate(90deg)";
+    }
+    else {
+        navbar.style.left = "-100%";
+        menu.style.transform = "rotate(0deg)";
+
+    }
+    isShow = !isShow;
+})
